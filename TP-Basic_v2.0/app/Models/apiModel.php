7c97649e105d367ba444fa5577f2ccddd8fc04e5
tba-adnan <tebbaa.adnane.dev@gmail.com>
2022-11-15 12:24:26 +0100

    add Proto Basic v2.0

---
+++ TP-Basic_v2.0/app/Models/apiModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class apiModel extends Model
+{
+    use HasFactory;
+    protected $table = 'students'; 
+    protected $fillable = ['name']; 
+}
