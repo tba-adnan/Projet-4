7e450c8d49a2cff9a0c6aaa8de77f018da79943a
tba-adnan <tebbaa.adnane.dev@gmail.com>
2022-11-16 16:49:26 +0100

    .

--- TP-Standard/app.js
+++ TP-Standard/app.js
@@ -1,9 +1,28 @@
-import React, { useState, useEffect } from 'react';
+var data = axios.get('http://127.0.0.1:8000/api/data').then(function(response){console.log(response.data);})
+data_one = data
+// console.log(data_one[0])
+
+console.log(data_one)
 
 
 
-ReactDOM.render(<h1>demo</h1>, document.getElementById('mydiv'))
+function process_array(array) {
+    var values_seen = {}; // for removing duplicates
+    for (var i = 0; i < array.length; i++) {
+        values_seen[array[i]["level"]] = true;
+    }
+    return Object.keys(values_seen);
+}
 
 
 
-useEffect(() => {fetch('http://127.0.0.1:8000/api/data').then((response) => console.log(response));}, []);
+
+
+// data_one.map(data => 
+//     <p>{data}</p>
+//     );
+
+// const myElement = <p>{data_one}</p>;
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(myElement);
