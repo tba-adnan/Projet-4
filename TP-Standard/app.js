1312e058b1da64df76edb44d4f04b33520e2b3e9
TebbaaX <>
2022-11-20 12:43:48 +0100

    fix.

--- TP-Standard/app.js
+++ TP-Standard/app.js
@@ -1,17 +1,19 @@
 // Sending Request and getting RAW data.
 var data = axios.get('http://127.0.0.1:8000/api/data').then(
     function(response){
-    console.log(response.data)
+    // console.log(response.data)
 // Loop tasks (Should switch to map function.)
     let items = response.data
     function Display() {
       let components = []
       let id = []
-      for (let i in items) {components.push(<li key={i}> Tâche : {items[i]['name']}</li>)} return components;}
+      for (let i in items) {components.push(
+      <li key={i}> Tâche : {items[i]['name']}</li>
+      )} return components;}
+
 // Render the Display.
       const root = ReactDOM.createRoot(document.getElementById('display'));
       root.render(<Display />);
-
 })
 
 
@@ -20,7 +22,6 @@ class CrudClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {value: ''};
-
     this.handleChange = this.handleChange.bind(this);
     this.addTask = this.addTask.bind(this);
   }
@@ -38,28 +39,24 @@ class CrudClass extends React.Component {
       console.log(response);
     })
 
-    // getUserData()
-    // alert(this.state.value)
-    // console.log(this.state.value)
     event.preventDefault();
   }
 
   render() {
     return (
+      
       <form onSubmit={this.addTask}>
         <label>
           <h2>Ajouté une Tâche :</h2>
           <br></br>
           <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
-        <br></br>
-        <input type="submit" value="Submit" />
+
+        <input type="submit" class="btn btn-primary" value="Submit" />
       </form>
+    
     );
-
-
   }
 }
-
 
 ReactDOM.render(<CrudClass />, document.getElementById('root'))
--- TP-Standard/junk.js
+++
@@ -1,9 +0,0 @@
-
-    // var data = [{
-    //     "ErrorCode":100,
-    //     "Message":{},
-    //     "Name":"InternetGatewayDevice.LANDevice.1.Hosts.HostNumberOfEntries",
-    //     "Value":"2"
-    //   }];
-      
-    //   console.log(data[0].Value);
